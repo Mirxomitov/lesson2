1a2046e5c5aa18d4283e2ca736ae32121b15ccb0
Tohir Mirxomitov <mirxtohir@gmail.com>
2026-05-11 20:23:12 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const requests = [];
+
+for (let i = 0; i < 10; i++) {
+  requests.push(
+    fetch("http://localhost:3000/buy", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 20 }),
+    })
+  );
+}
+
+Promise.all(requests)
+  .then(async (responses) => {
+    for (const response of responses) {
+      console.log(await response.json());
+    }
+  })
+  .catch(console.error);
